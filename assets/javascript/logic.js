a6f0c5cafee39a62da8d7ca21b86b1c48a27e58c
thescottbob <scottmcmains@gmail.com>
2018-10-02 20:51:20 -0700

    AddedOpenMenuAPICall

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -7,8 +7,9 @@
 // Valid Diet filters:           [balanced, high-protein, low-fat, low-carb]
 // Valid Health/Allergy filters: [vegan, vegetarian, sugar-conscious, peanut-free, tree-nut-free, alcohol-free]
 
-let edamAppID = "ed77f616"
-let edamApiKey = "976bea25ef3c24d77968f5c1879d9012"
+let edamAppID = "ed77f616";
+let edamApiKey = "976bea25ef3c24d77968f5c1879d9012";
+let openMenuApiKey = "3d7fcbb4-c412-11e8-b19e-525400552a35";
 let food = "";
 let dietRestrictions = [];
 let healthRestrictions = [];
@@ -31,53 +32,94 @@ $("#foodSubmit").on("click", function (event) {
         healthRestrictions.push("vegetarian")
     }
     
-    // Basic query
-    let edamURL = `https://api.edamam.com/search?app_id=${edamAppID}&app_key=${edamApiKey}&q=${food}`
-    // Add additional flags as necessary
-    if (healthRestrictions.length>0) {
-        // Only supporting Peanut allergy for now
-        if (healthRestrictions.contains("peanut-free")){
-            edamURL += "&health="+"peanut-free"
-        }
-    }
+    // // Basic query
+    // let edamURL = `https://api.edamam.com/search?app_id=${edamAppID}&app_key=${edamApiKey}&q=${food}`
+    // // Add additional flags as necessary
+    // if (healthRestrictions.length>0) {
+    //     // Only supporting Peanut allergy for now
+    //     if (healthRestrictions.contains("peanut-free")){
+    //         edamURL += "&health="+"peanut-free"
+    //     }
+    // }
 
-    // Edamam API call
-    $.ajax(edamURL, {
-        method: "GET"
-    }).then(function(stuff) {
-        recipes = stuff.hits
-        console.log(recipes)
+    // // Edamam API call
+    // $.ajax(edamURL, {
+    //     method: "GET"
+    // }).then(function(stuff) {
+    //     recipes = stuff.hits
+    //     console.log(recipes)
 
-        // Push recipe info to Recipes tab
-        $("#recipeData").empty()
+    //     // Push recipe info to Recipes tab
+    //     $("#recipeData").empty()
         
-        for (let i=0; i<recipes.length; i++) {
-            let r = recipes[i].recipe
-            console.log("-----------------------------")
-            console.log(r.label)
-            console.log(r.url)
-            console.log(r.ingredients)
-            console.log(r.healthLabels)
-            // $("body").append( $("<img>").attr("src",r.image) )
-            console.log("-----------------------------")
+    //     for (let i=0; i<recipes.length; i++) {
+    //         let r = recipes[i].recipe
+    //         console.log("-----------------------------")
+    //         console.log(r.label)
+    //         console.log(r.url)
+    //         console.log(r.ingredients)
+    //         console.log(r.healthLabels)
+    //         // $("body").append( $("<img>").attr("src",r.image) )
+    //         console.log("-----------------------------")
 
-            // Create new table row
-            let newRow = $("<div>").addClass("row recipeRow")
+    //         // Create new table row
+    //         let newRow = $("<div>").addClass("row recipeRow")
 
-            // Add recipe image
-            newRow.append($("<img>").addClass("col recipeImg").attr({
-                src: r.image,
-                alt: r.label
-            }))
-            // Add recipe name with hyperlink to source
-            newRow.append($("<a>").addClass("col recipeName").text(r.label).attr("href", r.url))
-            // Add health/diet labels
-            let healthDesc = r.healthLabels.join(", ")
-            newRow.append($("<div>").addClass("col recipeHealthLabel").text(healthDesc))
+    //         // Add recipe image
+    //         newRow.append($("<img>").addClass("col recipeImg").attr({
+    //             src: r.image,
+    //             alt: r.label
+    //         }))
+    //         // Add recipe name with hyperlink to source
+    //         newRow.append($("<a>").addClass("col recipeName").text(r.label).attr("href", r.url))
+    //         // Add health/diet labels
+    //         let healthDesc = r.healthLabels.join(", ")
+    //         newRow.append($("<div>").addClass("col recipeHealthLabel").text(healthDesc))
 
-            $("#recipeData").append(newRow)
-        }
-    })
+    //         $("#recipeData").append(newRow)
+    //     }
+    // })
+
+    // Basic OpenMenu query
+    // var postalCode = $("#zipCode").val();
+    var postalCode = 98105;
+    let openMenuURL = `https://openmenu.com/api/v2/search.php?key=${openMenuApiKey}&s=${food}&mi=1&postal_code=${postalCode}&country=US`;
+  
+  // OpenMenu API call
+  $.ajax(openMenuURL, {
+    method: "GET"
+  }).then(function(getRestaurantData) {
+    let restaurants = getRestaurantData.response.result.items;
+    console.log(getRestaurantData);
+
+    // Push restaurant info to Restaurants tab
+    $("#restaurantData").empty();
+
+    for (let i = 0; i < restaurants.length; i++) {
+      let res = restaurants[i];
+
+      // Create new table row
+      let newRow = $("<div>").addClass("row restaurantRow");
+
+      
+      newRow.append(
+        $("<div>")
+          .addClass("col menuItemName")
+          .text(res.menu_item_name)
+        //   .attr("href", website_url)
+      );
+
+      // Add restaurant name with hyperlink to source
+      newRow.append(
+        $("<a>")
+          .addClass("col restaurantName")
+          .text(res.restaurant_name)
+        //   .attr("href", website_url)
+      );
+
+      $("#restaurantData").append(newRow);
+    }
+  });
 })
 
 /************************************
